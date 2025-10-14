4cf022f81a79d19188e7c2444e49877e9bc6ca63
megarcrazy <strangernotjoke@hotmail.com>
2025-10-14 18:22:38 +1100

    CORS setup

--- server/app/app.py
+++ server/app/app.py
@@ -1,5 +1,6 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.router import backend_router
 
 
@@ -9,4 +10,9 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://localhost:3000", "http://localhost:3000"],
+    allow_methods=["GET", "POST", "PUT", "PATCH", "DELETE"],
+)
 app.include_router(backend_router)
