0f88445773ab1548ab814b5db37fc4b6c049192d
megarcrazy <strangernotjoke@hotmail.com>
2025-11-04 18:45:29 +1100

    Add 127.0.0.1 to CORS

--- server/app/app.py
+++ server/app/app.py
@@ -17,6 +17,8 @@ app.add_middleware(
     allow_origins=[
         "https://localhost:3000",
         "http://localhost:3000",
+        "http://127.0.0.1:3000",
+        "https://127.0.0.1:3000",
         f"http://{local_ip}:3000",
         f"https://{local_ip}:3000",
     ],
